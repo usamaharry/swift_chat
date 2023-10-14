ff55e743e612fefb0cd0d5a38a7eba53338658e1
Usama Javed <usama.javaid1919@gmail.com>
2023-10-15 04:51:29 +0500

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+//local
 import 'package:swift_chat/app/app.bottomsheets.dart';
 import 'package:swift_chat/app/app.dialogs.dart';
 import 'package:swift_chat/app/app.locator.dart';
 import 'package:swift_chat/app/app.router.dart';
-import 'package:stacked_services/stacked_services.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await setupLocator();
+
   setupDialogUi();
   setupBottomSheetUi();
+
   runApp(const MainApp());
 }
 
