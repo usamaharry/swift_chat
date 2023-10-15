7bb4e774765edd9371ed043ecf5d08eb89106c5e
Usama Javed <usama.javaid1919@gmail.com>
2023-10-15 05:24:52 +0500

    splash screen && icon

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
-import 'package:swift_chat/ui/common/ui_helpers.dart';
 
 import 'startup_viewmodel.dart';
 
@@ -14,32 +13,9 @@ class StartupView extends StackedView<StartupViewModel> {
     StartupViewModel viewModel,
     Widget? child,
   ) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'STACKED',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Loading ...', style: TextStyle(fontSize: 16)),
-                horizontalSpaceSmall,
-                SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 6,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+        child: Image.asset('assets/images/logo.png'),
       ),
     );
   }
