e372d15b86b21361988aed2a5ac385b8fa012217
Usama Javed <usama.javaid1919@gmail.com>
2023-10-15 14:48:40 +0500

    remove debugged banner

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: Routes.startupView,
       onGenerateRoute: StackedRouter().onGenerateRoute,
       navigatorKey: StackedService.navigatorKey,
